337111eb9315bd8b9f9e026b54377753b549b5b9
ngluutu <ngluutu@gmail.com>
2022-09-22 23:26:58 +0700

    Update in W3

---
+++ week3_basicapp/lib/form.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FormText {
+  String id; //textfield
+  String school; //dropdownbutton
+  String graduate;
+
+  FormText(this.id, this.school, this.graduate);
+}
